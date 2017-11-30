e74d4372687bfc57df8952385a581c9f6fa87329
sam <snaumann2@gmail.com>
2017-11-30 18:34:23 -0500

    is_Full()
    Added Is_Full method to board and added implements keyListener and implements actionListener into class comment

--- 2048.2/src/Board.java
+++ 2048.2/src/Board.java
@@ -2,9 +2,28 @@ import java.awt.*;
 
 public class Board extends Canvas {
 	
+	//instance variables
+	Tile[][] board = new Tile[4][4];
+	boolean board_full = false;
+	
+	//constructor
 	Board(){
 		setSize(500, 500);
 		setBackground(Color.darkGray);
+	}
+	
+	//methods
+	public boolean is_Full() {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (board[i][j] == null) {
+					return false;
+				} else {
+					continue;
+				}
+			}
+		}
+		return true;
 	}
 
 }
--- 2048.2/src/main.java
+++ 2048.2/src/main.java
@@ -8,24 +8,12 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*; 
 
-public class main extends Applet {//implements ActionListener 
+public class main extends Applet{ // implements ActionListener implements KeyListener
 
 	public void init() {
 		
 		Board mainBoard = new Board();
-		add(mainBoard);
 		
-		setBackground(Color.red);
-		Tile tile = new Tile(1) ;
-		mainBoard.add(tile);
-		Tile tile2 = new Tile(2) ;
-		mainBoard.add(tile2);
-		Tile tile3 = new Tile(3) ;
-		mainBoard.add(tile3);
-		Tile tile4 = new Tile(4) ;
-		mainBoard.add(tile4);
-		Tile tile5 = new Tile(5) ;
-		mainBoard.add(tile5);
 		
 		
 		
