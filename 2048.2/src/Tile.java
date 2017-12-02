581e3f4d093103583ff79b0306529b76ac684eff
sbnaumann <33404072+sbnaumann@users.noreply.github.com>
2017-12-01 20:47:33 -0500

    Revert "Merge branch 'master' into sam"
    This reverts commit 6f7d0a0aaffc2599db44856c9e8cf9239902d71f, reversing
    changes made to 5d4fcf56ee35ec5bd368ac10bb388b453baf1d37.

--- 2048.2/src/Tile.java
+++ 2048.2/src/Tile.java
@@ -1,6 +1,4 @@
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.lang.*;
 
 public class Tile extends Canvas {
 	static final Color TILE_2_COLOR = new Color(236,218,195);
@@ -20,28 +18,9 @@ public class Tile extends Canvas {
 	static final Color TILE_32768_COLOR = new Color();
 	static final Color TILE_65536_COLOR = new Color();*/
 	
-<<<<<<< HEAD
 	Tile(int powerOfTwo) {
 		setSize(100, 100);
-=======
-	static final double TILE_RATIO = 0.2;
-	static final double TILE_SPACING = (1 - (TILE_RATIO * 4))/5;
-	Color tileColor;
-	int titleText;
-	int xCoord;
-	int yCoord; 
-	int power;
-	boolean justMoved = false;
-	
-	Tile(int powerOfTwo, int xCoord, int yCoord) {
-		setSize((int)(main.BOARD_SIZE * TILE_RATIO), (int)(main.BOARD_SIZE * TILE_RATIO));
->>>>>>> master
 		setBackground(colorFromPower(powerOfTwo));
-		tileColor = colorFromPower(powerOfTwo);
-		titleText = (int) Math.pow(2, powerOfTwo);
-		this.xCoord = xCoord;
-		power = powerOfTwo;
-		this.yCoord = yCoord;
 	}
 
 
@@ -57,175 +36,6 @@ public class Tile extends Canvas {
 		}
 	}
 	
-	public void canMove(char c) {
-		switch(c) {
-		case 'w':{
-			while(canMoveUp()) {
-	  			System.out.println("Can move");
-	  			Tile[][] tempArray = Board.board;
-	  			if(Board.board[yCoord - 1][xCoord] != null && Board.board[yCoord - 1][xCoord].power == this.power) {
-	  				this.power++;
-	  			}
-	  				Board.board[yCoord - 1][xCoord] = this;
-	  				Board.board[yCoord][xCoord] = null;
-	  			yCoord -= 1;
-	  			printArray(Board.board);
-	  		}
-			
-			break;
-		}
-		case 's':{
-			while(canMoveDown()) {
-	  			System.out.println("Can move");
-	  			Tile[][] tempArray = Board.board;
-	  			if(tempArray[yCoord + 1][xCoord] != null && tempArray[yCoord + 1][xCoord].power == tempArray[yCoord][xCoord].power) {
-	  				tempArray[yCoord + 1][xCoord] = new Tile(this.power + 1, yCoord + 1, xCoord);
-	  			}else {
-		  			tempArray[yCoord + 1][xCoord] = tempArray[yCoord][xCoord];
-		  		}
-	  			tempArray[yCoord][xCoord] = null;
-	  			yCoord += 1;
-	  			Board.board = tempArray;
-	  			printArray(Board.board);
-	  		}
-			break;
-		}
-
-		case 'd':{
-			while(canMoveRight()) {
-	  			System.out.println("Can move");
-	  			Tile[][] tempArray = Board.board;
-	  			tempArray[yCoord][xCoord + 1] = this;
-	  			tempArray[yCoord][xCoord] = null;
-	  			xCoord += 1;
-	  			Board.board = tempArray;
-	  			printArray(Board.board);
-	  		}
-			break;
-		}
-		
-
-		case 'a':{
-
-			while(canMoveLeft()) {
-	  			System.out.println("Can move");
-	  			Tile[][] tempArray = Board.board;
-	  			tempArray[yCoord][xCoord - 1] = this;
-	  			tempArray[yCoord][xCoord] = null;
-	  			xCoord -= 1;
-	  			Board.board = tempArray;
-	  			printArray(Board.board);
-	  		}
-			break;
-		}
-		
-
-		case 'q':{
-
-			while(canMoveUp() && canMoveLeft()) {
-	  			System.out.println("Can move");
-	  			Tile[][] tempArray = Board.board;
-	  			tempArray[yCoord - 1][xCoord - 1] = this;
-	  			tempArray[yCoord][xCoord] = null;
-	  			xCoord -= 1;
-	  			yCoord -= 1;
-	  			Board.board = tempArray;
-	  			printArray(Board.board);
-	  		}
-			break;
-		}
-
-
-		case 'e':{
-
-			while(canMoveUp() && canMoveRight()) {
-	  			System.out.println("Can move");
-	  			Tile[][] tempArray = Board.board;
-	  			tempArray[yCoord - 1][xCoord + 1] = this;
-	  			tempArray[yCoord][xCoord] = null;
-	  			xCoord += 1;
-	  			yCoord -= 1;
-	  			Board.board = tempArray;
-	  			printArray(Board.board);
-	  		}
-			break;
-		}
-		
-		case 'c':{
-
-			while(canMoveDown() && canMoveRight()) {
-	  			System.out.println("Can move");
-	  			Tile[][] tempArray = Board.board;
-	  			tempArray[yCoord + 1][xCoord + 1] = this;
-	  			tempArray[yCoord][xCoord] = null;
-	  			xCoord += 1;
-	  			yCoord += 1;
-	  			Board.board = tempArray;
-	  			printArray(Board.board);
-	  		}
-			break;
-		}
-		
-		case 'z':{
-
-			while(canMoveDown() && canMoveLeft()) {
-	  			System.out.println("Can move");
-	  			Tile[][] tempArray = Board.board;
-	  			tempArray[yCoord + 1][xCoord - 1] = this;
-	  			tempArray[yCoord][xCoord] = null;
-	  			xCoord -= 1;
-	  			yCoord += 1;
-	  			Board.board = tempArray;
-	  			printArray(Board.board);
-	  		}
-			break;
-		}
-			
-		}
-	     
-			main.mainBoard.repaint();
-
-	    System.out.println(c);
-	      
-	}
-
-	public boolean canMoveLeft() {
-		return ((!(xCoord - 1 < 0)) && 
-				((Board.board[yCoord][xCoord - 1] == null)// If the sqaure above is null
-				|| Board.board[yCoord][xCoord - 1].power == this.power)
-				);
-		
-	}
-	public boolean canMoveRight() {
-		return ((!(xCoord + 1 > 3)) && 
-				((Board.board[yCoord][xCoord + 1] == null)// If the sqaure above is null
-				|| Board.board[yCoord][xCoord + 1].power == this.power)
-				);
-	}
-	public boolean canMoveUp() {
-		return ((!(yCoord - 1 < 0)) && //If not out of bounds
-				((Board.board[yCoord - 1][xCoord] == null)// If the sqaure above is null
-				|| Board.board[yCoord - 1][xCoord].power == this.power)
-				);
-		
-	}
-	public boolean canMoveDown() {
-		return ((!(yCoord + 1 > 3)) && 
-				((Board.board[yCoord + 1][xCoord] == null)// If the sqaure above is null
-				|| Board.board[yCoord + 1][xCoord].power == this.power)
-				);
-	}
-	
-	public static void printArray(Tile[][] matrix) {
-	    for (int row = 0; row < matrix.length; row++) {
-	        for (int column = 0; column < matrix[row].length; column++) {
-	            System.out.print(matrix[row][column] + " ");
-	        }
-	        System.out.println();
-	    }
-	}
-	
-
 
 	
 }
